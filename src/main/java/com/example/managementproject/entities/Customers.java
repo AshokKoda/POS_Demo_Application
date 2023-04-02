71ba4394fd6b7807f75ff0e24a4119bf4bed403b
AshokKoda <a.koda59@gmail.com>
2023-04-02 08:44:58 +0530

    Added:created Customer entity and database configuration done

---
+++ src/main/java/com/example/managementproject/entities/Customers.java
@@ -0,0 +1,126 @@
+package com.example.managementproject.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customers")
+public class Customers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "regNo")
+    private String regNo;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "emailId")
+    private String emailId;
+
+    @Column(name = "phoneNo")
+    private String phoneNo;
+
+    @Column(name = "dob")
+    private Date dob;
+
+    @Column(name = "createdOn")
+    private Date createdOn;
+
+    public Customers() {
+    }
+
+    public Customers(int id, String regNo, String firstName, String lastName, String emailId, String phoneNo, Date dob,
+            Date createdOn) {
+        this.id = id;
+        this.regNo = regNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+        this.phoneNo = phoneNo;
+        this.dob = dob;
+        this.createdOn = createdOn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    @Override
+    public String toString() {
+        return "Customers [id=" + id + ", regNo=" + regNo + ", firstName=" + firstName + ", lastName=" + lastName
+                + ", emailId=" + emailId + ", phoneNo=" + phoneNo + ", dob=" + dob + ", createdOn=" + createdOn + "]";
+    }
+
+}
